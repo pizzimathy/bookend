9d4c074af7c8690276ba8b0208e22af7615857df
apizzimenti <pizzimentianthony@gmail.com>
2016-10-22 14:10:15 -0500

    add entry_points

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="booklist",
     description="Keeps track of the books you want to read!",
 
     version="0.0.1",
-    license="MIT"
+    license="MIT",
+
+    url="https://github.com/apizzimenti/booklist.git",
+    author="Anthony Pizzimenti",
+    author_email="pizzimentianthony@gmail.com",
+
+    packages="booklist",
+
+    entry_points={
+        "console_scripts": ["booklist=booklist.index:main"]
+    }
 )
